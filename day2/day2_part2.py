d7ffdc834be6752098701c444e643f6d1e9b08f0
Joel Bonini <joel.bonini@santismail.ch>
2024-12-11 12:31:28 +0100

    day2

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -1,77 +1,60 @@
 from day2_part1 import parseInput, increaseCheck 
 
 
-def rule1(input, tolerance):
+def rule1(input):
     increase = increaseCheck(input)
     control = input[0]
-    i = 0
-    while i < len(input):
-        if increase:
-                if input[i] < control:
-                    if tolerance == 0:
-                        return False
-                    else:
-                        tolerance = 0
-                        input.pop(i)
-                        rule1(input, tolerance)
-                else:
-                    control = input[i]
-            
-        else:
-                if input[i] > control:
-                    if tolerance == 0:
-                        return False
-                    else:
-                        tolerance = 0
-                        input.pop(i)
-                        rule1(input, tolerance)
-                else:
-                    control = input[i]
-        i += 1
-            
-    result = rule2(input, tolerance)
-    return result
-
-
-
-def rule2(input, tolerance):
-    increase = increaseCheck(input)
-    i = 0
-    while i < len(input) - 1:
-        if increase:
-            if (input[i] - input[i + 1]) * -1 > 3 or (input[i] - input[i + 1]) * -1 < 1:
-                if tolerance == 0:
+    for num in input:
+        match increase:
+            case True:
+                if num < control:
                     return False
                 else:
-                    tolerance = 0
-                    input.pop(i)
-                    rule2(input, tolerance)
-
-        else:
-            if input[i] - input[i + 1] > 3 or input[i] - input[i + 1] < 1:
-                if tolerance == 0:
+                    control = num
+            
+            case False:
+                if num > control:
                     return False
                 else:
-                    tolerance = 0
-                    input.pop(i)
-                    rule2(input, tolerance)
-        
-        i += 1
-
+                    control = num
+            
     return True
 
 
 
+def rule2(input):
+    increase = increaseCheck(input)
+    for i in range(len(input) - 1):
+        match increase:
+            case True:
+                if (input[i] - input[i + 1]) * -1 > 3 or (input[i] - input[i + 1]) * -1 < 1:
+                    return False
+            case False:
+                if input[i] - input[i + 1] > 3 or input[i] - input[i + 1] < 1:
+                    return False
+    return True
+
+
+
+def check_report(input):
+    for num in input:
+        templist = input
+        templist.remove(num)
+        result = rule1(templist)
+        if result == False:
+            return False
+    return True
+            
+
 
 def main(input):
     safe = 0
-    tolerance = 1
     for report in input:
-        result = rule1(report, tolerance)
+        result = check_report(report)
         if result == True:
             safe += 1
 
-    print(safe)            
+    print(safe)          
     
     
 
