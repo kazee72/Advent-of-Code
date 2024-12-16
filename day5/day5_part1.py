629e53a43a8998b81e14ec368455f405a41976f0
Joel Bonini <joel.bonini@santismail.ch>
2024-12-16 13:40:19 +0100

    day5

--- day5/day5_part1.py
+++ day5/day5_part1.py
@@ -30,18 +30,15 @@ def parse_input():
 
 
 
-def includes(rule, update):
-    counter = 0
-    for num in rule:
-        if num in update:
-            counter += 1
-        
-    return counter == 2
+
     
         
 
-def check_rule(rule, update):
-    return update.index(rule[0]) < update.index(rule[1])
+def check(rules, update):
+    for rule in rules:
+        if rule[0] in update and rule[1] in update and update.index(rule[0]) > update.index(rule[1]):
+            return False
+    return True
 
 
 
@@ -49,18 +46,14 @@ def main(input):
     rules = input[0]
     updates = input[1]
 
+    results = []
+
     for update in updates:
-        counter = 0
-        for rule in rules:
-            include = includes(rule, update)
-            if include:
-                pass_rule = check_rule(rule, update)
-                if pass_rule:
-                    counter += 1
-                else:
-                    break
-            if counter == len(rules):
-                print("Rule ", rules.index(rule), " passes")
+        result = check(rules, update)
+        results.append(result)
+
+    print(results)
+
 
 
 
