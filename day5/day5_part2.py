4a63f9b1436c83c6b6a8e56ccb7bbe4e965e01a3
Joel Bonini <joel.bonini@santismail.ch>
2024-12-17 10:31:47 +0100

    day5

---
+++ day5/day5_part2.py
@@ -0,0 +1,56 @@
+from day5_part1 import parse_input
+
+
+def check(rules, update):
+    for rule in rules:
+        if rule[0] in update and rule[1] in update and update.index(rule[0]) > update.index(rule[1]):
+            return update.index(rule[0]), update.index(rule[1])
+    return True
+
+
+
+def switch(update, indices):
+    temp = [update[indices[0]], update[indices[1]]]
+    update[indices[0]] = temp[1]
+    update[indices[1]] = temp[0]
+
+    return update
+
+
+
+
+def main(input):
+    rules = input[0]
+    updates = input[1]
+
+    values = []
+    
+    for update in updates:
+        switched = False
+        check_result = check(rules, update)
+        while check_result != True:
+            update = switch(update, check_result)
+            check_result = check(rules, update)
+            switched = True
+        
+        if switched:
+            values.append(update[len(update)// 2 + 1])
+        
+    result = 0
+    for value in values:
+        result += int(value)
+
+    print(result)
+
+        
+
+
+    
+    
+
+
+
+
+if __name__ == "__main__":
+    input = parse_input()
+    main(input)
